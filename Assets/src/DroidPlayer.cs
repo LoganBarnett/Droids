f7e6a739c4cbd2c4505a72da8978a26b447d9a37
Logan Barnett <logustus@gmail.com>
2010-10-07 07:56:31 -0700

    Forgot to add some files.

---
+++ Assets/src/DroidPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class DriodPlayer : MonoBehaviour {
+	Color color;
+	
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
---
+++ Assets/src/DroidPlayerManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class DroidPlayerManager : MonoBehaviour {
+	
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+	
+	void OnPlayerConnected(NetworkPlayer player) {
+		
+	}
+	
+	void OnServerInitialized() {
+		// create player
+	}
+}
